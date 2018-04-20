37cf28214277aaa069b16edebfb10078409464e7
Joe <hoangjd@vcu.edu>
2018-04-20 17:26:35 -0400

    UI Changes based on Users

--- Quiz Me/Quiz Me/GameViewController.swift
+++ Quiz Me/Quiz Me/GameViewController.swift
@@ -11,30 +11,67 @@ import MultipeerConnectivity
 
 class GameViewController: UIViewController, MCSessionDelegate {
     
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    
+    
+    
+    @IBOutlet var arrayOfUserChoices: [UILabel]!
+    
+    
+    @IBOutlet var arrayOfUserScores: [UILabel]!
+    
+//    @IBOutlet weak var user1ChoiceLabel: UILabel!
+//    @IBOutlet weak var user1ScoreLabel: UILabel!
+//    @IBOutlet weak var user2ChoiceLabel: UILabel!
+//    @IBOutlet weak var user2ScoreLabel: UILabel!
+//    @IBOutlet weak var user3ChoiceLabel: UILabel!
+//    @IBOutlet weak var user3ScoreLabel: UILabel!
+//    @IBOutlet weak var user4ChoiceLabel: UILabel!
+//    @IBOutlet weak var user4ScoreLabel: UILabel!
+//
+    @IBOutlet weak var questionNumLabel: UILabel!
+    @IBOutlet weak var questionText: UITextView!
+    
+    @IBOutlet weak var winLabel: UILabel!
+    
+    @IBOutlet weak var restartButton: UIButton!
+    
     var session: MCSession!
     var peerID: MCPeerID!
     
 
-    @IBOutlet var ArrayOfImageViews: [UIImageView]!
+    @IBOutlet var arrayOfImages: [UIImageView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(session.connectedPeers.count)
-        setUpPlayersImages()
+        initializeValues()
         
         // Do any additional setup after loading the view.
         
     }
     
+    func initializeValues() {
+        restartButton.isHidden = true
+        winLabel.isHidden = true
+        setUpPlayersImages()
+    }
+    
     
     
     func setUpPlayersImages() {
-        for i in 1..<ArrayOfImageViews.count {
-            ArrayOfImageViews[i].isHidden = true
+        for i in 1..<arrayOfImages.count {
+            arrayOfImages[i].isHidden = true
+            arrayOfUserChoices[i].isHidden = true
+            arrayOfUserScores[i].isHidden = true
         }
         for i in 1...session.connectedPeers.count {
-            ArrayOfImageViews[i].isHidden = false
+            arrayOfImages[i].isHidden = false
+            arrayOfUserChoices[i].isHidden = false
+            arrayOfUserScores[i].isHidden = false
         }
 
     }
