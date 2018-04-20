314720c56e2104af805d081885b0da90eb052e8e
Joe <hoangjd@vcu.edu>
2018-04-20 12:58:35 -0400

    Added transport security to plist

--- Quiz Me/Quiz Me/SingleGameViewController.swift
+++ Quiz Me/Quiz Me/SingleGameViewController.swift
@@ -12,8 +12,42 @@ class SingleGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getJSONData()
         // Do any additional setup after loading the view.
+    }
+    
+    func getJSONData(){
+        
+        let urlString = "http://www.people.vcu.edu/~ebulut/jsonFiles/quiz1.json"
+        
+        
+        let url = URL(string: urlString)
+        
+        let session = URLSession.shared
+        
+        // create a data task
+        let task = session.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            if let result = data{
+                
+                print("inside get JSON")
+                print(result)
+                do{
+                    let json = try JSONSerialization.jsonObject(with: result, options: .allowFragments)
+                    
+                    if let dictionary = json as? [String:Any]{
+                        print(dictionary["numberOfQuestions"])
+                    }
+                }
+                catch{
+                    print("Error")
+                }
+            }
+            
+        })
+        
+        // always call resume() to start
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
